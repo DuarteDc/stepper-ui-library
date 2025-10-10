1b073f3bd48295cfe22ca4c6bdd74617c7c09270
DuarteDc <al221910409@gmail.com>
2025-10-09 23:45:12 -0600

    test: fix basic test

--- src/test/useStepper.test.tsx
+++ src/test/useStepper.test.tsx
@@ -1,37 +1,64 @@
-import { beforeAll, describe, expect, it } from 'vitest'
-import { type StepComponentProps } from '../interfaces/step'
-import { stepsComponentMock } from './mocks/step-component-mock'
-import { useStepper } from '../hooks/useStepper'
 import { act, renderHook, waitFor } from '@testing-library/react'
+import { describe, expect, it, beforeEach, vi } from 'vitest'
+import { type StepComponentProps, type ValidateStep } from '../interfaces/step'
+import { useStepper } from '../hooks/useStepper'
+
+const createStepsMock = (): StepComponentProps[] => [
+  {
+    name: 'Step One',
+    component: vi.fn(),
+    icon: null
+  },
+  {
+    name: 'Step Two',
+    component: vi.fn(),
+    icon: null
+  },
+  {
+    name: 'Step Three',
+    component: vi.fn(),
+    icon: null
+  }
+]
 
 describe('useStepper Hook', () => {
   let mockComponent: StepComponentProps[]
-  let useStepperHook: ReturnType<typeof useStepper>
 
-  beforeAll(() => {
-    mockComponent = stepsComponentMock
-    const { result } = renderHook(() =>
-      useStepper({ stepsComponent: mockComponent })
-    )
-    useStepperHook = result.current
+  beforeEach(() => {
+    mockComponent = createStepsMock()
   })
 
   it('should initialize the hook correctly', () => {
-    expect(useStepperHook).toBeDefined()
-    expect(useStepperHook.step).toBe(0)
-    expect(useStepperHook.componentName).toBe('Step One')
-    expect(useStepperHook.Component).toBeDefined()
-    expect(typeof useStepperHook.nextStep).toBe('function')
-    expect(typeof useStepperHook.backStep).toBe('function')
-    expect(typeof useStepperHook.navigateTo).toBe('function')
-    expect(typeof useStepperHook.goToInitialStep).toBe('function')
+    const { result } = renderHook(() =>
+      useStepper({ stepsComponent: mockComponent })
+    )
+
+    expect(result.current).toBeDefined()
+    expect(result.current.step).toBe(0)
+    expect(result.current.componentName).toBe('Step One')
+    expect(result.current.Component).toBeDefined()
+    expect(typeof result.current.nextStep).toBe('function')
+    expect(typeof result.current.backStep).toBe('function')
+    expect(typeof result.current.navigateTo).toBe('function')
+    expect(typeof result.current.goToInitialStep).toBe('function')
   })
 
-  it('should go to the next step', async () => {
-    act(async () => await useStepperHook.nextStep())
+  it('should not advance if canContinue() result false', async () => {
+    const { result } = renderHook(() =>
+      useStepper({ stepsComponent: mockComponent })
+    )
+
+    result.current.stepRef.current = {
+      canContinue: vi.fn().mockResolvedValue(false)
+    } as unknown as ValidateStep
+
+    await act(async () => {
+      await result.current.nextStep()
+    })
+
     await waitFor(() => {
-      expect(useStepperHook.step).toBe(1)
-      expect(useStepperHook.componentName).toBe('Step Two')
+      expect(result.current.step).toBe(0)
+      expect(result.current.componentName).toBe('Step One')
     })
   })
 })
